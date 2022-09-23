a0bdc838fdf19f7816be25b12938248b67e4666d
bbayong28 <ghkdud5795@hanmail.net>
2022-09-23 12:53:34 +0900

    20220923

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <div className="container">
-        <i className="xi-instagram"></i>
+        <div className='logo'>
+          <img src={process.env.PUBLIC_URL+ '/assets/images/logo_w.png'} alt="" />
+        </div>
         <div className="top">
           <ul>
             <li><a href="/">COMPANY </a></li>
@@ -15,11 +17,20 @@ const Footer = () => {
           </ul>
         </div>
         <div className="middle">
+          <div className="top">
             <span>COMPANY 한아조 주식회사</span>
             <span>OWNER 김상만, 조한아</span>
             <span> TEL 070-7727-7117</span>
             <span>LUNCH BREAK 12:00~13:00</span>
-            <span>E-MAIL hanah@hanahzo.com</span>
+          <span>E-MAIL hanah@hanahzo.com</span>
+          </div>            
+          <div className="middle">
+            <span>BUSINESSNUMBER 527-86-02282</span>             
+            <span>MAIL-ORDER LICENSE 제 2021-서울노원-1980 호 [사업자정보확인]</span>
+          </div>
+          <div className="bottom">
+            <span>ADDERES 01684 서울특별시 노원구 상계로9가길 22 (상계동) 1층</span>
+          </div>
         </div>
         <div className="copy">
           <p>Copyright © 한아조. All rights reserved.</p>
